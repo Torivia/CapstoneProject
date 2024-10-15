5bce868cd647fa789b178f864c3830226c134dd8
Torivia <tlvargasrubio@gmail.com>
2024-10-15 13:14:34 -0700

    test

---
+++ src/main/java/com/pluralsight/CapstoneProject.java
@@ -0,0 +1,14 @@
+package com.pluralsight;
+
+public class CapstoneProject {
+    public static void main(String[] args) {
+
+       import java.time.LocalDateTime; import java.time.format.DateTimeFormatter; public class Program { public static void main(String[] args) { LocalDateTime today = LocalDateTime.now(); System.out.println("Today is: " + today); // Specify the date/time format you will want to use DateTimeFormatter fmt = DateTimeFormatter.ofPattern("E, MMM dd, yyyy HH:mm:ss"); String formattedDate = today.format(fmt); System.out.println("Today is: " + formattedDate); } }
+        }
+        }
+        ArrayList<Product> products = new ArrayList<Product>();
+
+        Product p1 = new Product(1,"Iphone 15", 999);
+        products.add(p1);
+        /wjdhfdjf
+    }
